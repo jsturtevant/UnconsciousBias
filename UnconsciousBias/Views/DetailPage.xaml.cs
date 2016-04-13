f6704fe7ce3d67a3cc6d5d698b7b95783c547bfb
James Sturtevant <jsturtevant@gmail.com>
2016-04-13 13:33:31 -0400

    intial commit. template 10

---
+++ UnconsciousBias/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using UnconsciousBias.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace UnconsciousBias.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ UnconsciousBias/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using UnconsciousBias.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace UnconsciousBias.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
